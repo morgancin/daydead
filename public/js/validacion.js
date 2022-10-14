21829fdd4cf511f3f6d9bf74b1ddaeb63a55b0c8
loretvi <lovil.trinidad@gmail.com>
2022-10-14 15:03:54 -0500

    Arreglo del landing

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,6 +1,1 @@
-var n = 0;
-  var l = document.getElementById("contador");
-  window.setInterval(function(){
-       l.innerHTML = n;
-          n++;
-          },1000);
+
--- resources/views/website/register.blade.php
+++ resources/views/website/register.blade.php
@@ -31,9 +31,9 @@
             @endif
            <!-- Fin  -->
            <!--Contador de segundos -->
-           <script type="">
-                var n = 0;
-                var l = document.getElementById("contador");
+           <script type="text/javascript">
+                var n ='0';
+                var l = document.getElementById('contador');
                 window.setInterval(function(){
                      l.innerHTML = n;
                         n++;
