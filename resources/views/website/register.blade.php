f70b2335c754473936b491fe37fc5426effeb19f
loretvi <lovil.trinidad@gmail.com>
2022-10-18 16:00:47 -0500

    Cambio de select

--- resources/views/website/register.blade.php
+++ resources/views/website/register.blade.php
@@ -3,13 +3,12 @@
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-        <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-        <script src="{{ asset('js/validacion.js') }}"></script>
-      <title>Alcis Corp</title>
+      <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+     <!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> -->
+     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+     <link href="{{ asset('css/select2.css') }}" rel="stylesheet">
+     <title>Alcis Corp</title>
     </head>
       <body class="bg-fixed overflow-auto" style="background-image:url({{ asset('imagenes/fondo_petalos.png') }})">
         <img class="justify-start  w-auto" src="{{ asset('imagenes/Papel_picado.png') }}" alt="">
@@ -110,67 +109,6 @@
                                 </label>
                             </div>
                         </div>
-                        <script>
-                            $(document).ready(function() {
-                                $('.select2').select2();
-                            });
-                        </script>
-                        <style>
-                            js-example-basic-single::colores{
-                                background: #701a75;
-                            }
-                        </style>
-                        @if ($cSelectPlace)
-                            <div class="-mx-3 md:flex mb-6 justify-center items-center">
-                                <div class="md:w-1/2 px-3 mb-1 md:mb-0">
-                                    <select style=""
-                                    name="cmbPlace" id="panteon" required
-                                           style="background:#701a75" class="
-                                                    js-example-basic-single:color
-                                                    font-sans
-                                                    focus:border-yellow-400
-                                                    focus:outline-none
-                                                    focus:shadow-md
-                                                    rounded-lg
-                                                    bg-fuchsia-900 w-full
-                                                    border
-                                                    border-white
-                                                    text-white
-                                                    py-3 px-4 pr-8
-                                                    rounded-lg select2" >
-                                        <option value="">Selecciona lugar</option>
-                                        @foreach($oPlaces as $place)
-                                            <option  style="background:#701a75" value="{{ $place->id }}" {{ (int) old('cmbPlace') === (int) $place->id ? 'selected' : '' }}>{{ $place->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                        @endif
-
-                        @if ($cSelectBusiness)
-                            <div class="-mx-3 md:flex mb-6 justify-center items-center  ">
-                                <div class="md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <select name="cmbBusiness" id="sucursal" required
-                                            class=" focus:border-yellow-400
-                                                    focus:outline-none
-                                                    focus:shadow-md
-                                                    rounded-lg
-                                                    bg-fuchsia-900
-                                                    w-full
-                                                    border
-                                                    border-white
-                                                    text-white
-                                                    py-3 px-4 pr-8
-                                                    rounded-lg">
-                                        <option value="">Selecciona linea de negocio</option>
-                                        <option value="PF" {{ old('cmbBusiness') === 'PF' ? 'selected' : '' }}>PREVENCIÓN FINAL</option>
-                                        <option value="SG" {{ old('cmbBusiness') === 'SG' ? 'selected' : '' }}>SANTA GLORIA</option>
-                                        <option value="BF" {{ old('cmbBusiness') === 'BF' ? 'selected' : '' }}>BYE BYE FRIEND</option>
-                                    </select>
-                                </div>
-                            </div>
-                        @endif
-
                         <div class="-mx-3 md:flex mb-6 justify-center items-center">
                             <div class="md:w-1/2 px-3 mb-1 md:mb-0">
                                 <label class="relative block">
@@ -201,6 +139,55 @@
                                 </label>
                             </div>
                         </div>
+                        @if ($cSelectPlace)
+                           <div class="-mx-3 md:flex mb-6 justify-center items-center">
+                                <div class="md:w-1/2 px-3 mb-1 md:mb-0">
+                                    <select
+                                    name="cmbPlace" id="panteon" required
+                                    class=" select2  body font-sans
+                                    focus:border-yellow-400
+                                    focus:outline-none
+                                    focus:shadow-md
+                                    rounded-lg
+                                    bg-fuchsia-900 w-full
+                                    border
+                                    border-white
+                                    text-white
+                                    py-3 px-4 pr-8
+                                    rounded-lg  " >
+                                        <option value="">Selecciona lugar</option>
+                                        @foreach($oPlaces as $place)
+                                            <option value="{{ $place->id }}" {{ (int) old('cmbPlace') === (int) $place->id ? 'selected' : '' }}>{{ $place->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        @endif
+
+                        @if ($cSelectBusiness)
+                            <div class="-mx-3 md:flex mb-6 justify-center items-center  ">
+                                <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                                    <select name="cmbBusiness" id="sucursal" required
+                                            class=" focus:border-yellow-400
+                                                    focus:outline-none
+                                                    focus:shadow-md
+                                                    rounded-lg
+                                                    bg-fuchsia-900
+                                                    w-full
+                                                    border
+                                                    border-white
+                                                    text-white
+                                                    py-3 px-4 pr-8
+                                                    rounded-lg">
+                                        <option value="">Selecciona linea de negocio</option>
+                                        <option value="PF" {{ old('cmbBusiness') === 'PF' ? 'selected' : '' }}>PREVENCIÓN FINAL</option>
+                                        <option value="SG" {{ old('cmbBusiness') === 'SG' ? 'selected' : '' }}>SANTA GLORIA</option>
+                                        <option value="BF" {{ old('cmbBusiness') === 'BF' ? 'selected' : '' }}>BYE BYE FRIEND</option>
+                                    </select>
+                                </div>
+                            </div>
+                        @endif
+
 
                         <div class="">
                             <div class=" flex  flex-4 justify-center items-center  ">
@@ -219,7 +206,16 @@
                         <img class="object-contain  h-24 w-full w-full" src="{{ asset("imagenes/cempasuchil.png") }}" alt="">
                     </div>
                 </div>
-            </body>
+
+            <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+            <script src="{{ asset('js/validacion.js') }}"></script>
+            <script>
+                $(document).ready(function() {
+                    $('.select2').select2();
+                });
+            </script>
+        </body>
     </html>
 
 <!--
