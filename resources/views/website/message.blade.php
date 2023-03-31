5514d532d8b212070eb77470f4cdc9b896f29048
Lore Vilchis <114778200+loretvi@users.noreply.github.com>
2023-03-31 13:07:02 -0600

    Update message.blade.php
    cambio de mensaje

--- resources/views/website/message.blade.php
+++ resources/views/website/message.blade.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-        <title>Confirmación de Registro</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <body class=" bg-fixed " style="background-image:url({{ asset('imagenes/fondo_petalos.png') }})">
-        <img src="imagenes/Papel_picado.png" alt="">
-        <div class="h-20 flex">
-            <img class="object-contain  h-10  w-full" src="{{ asset('imagenes/izquierdo.png') }}" alt="">
-            <img class="object-contain  h-10  w-full" src="{{ asset('imagenes/derecho.png') }}" alt="">
-        </div>
-        <div class=" bg-fixed h-15 flex">
-            <img class="object-contain  h-10  w-full" src="{{ asset('imagenes/Group 1.png') }}" alt="">
-        </div>
-        <div class=" bg-fixed px-8 pt-6 pb-8 mb-4 flex flex-col">
-            <div class="-mx-3 md:flex mb-6"><h1 class="text-white text-center">{{ $oLead->name }}</h1></div>
-            <div><h5 class="text-white text-center font-bold font-sans ">¡Gracias por registrarte! <br> pronto nos comunicaremos contigo.</h5></div>
-        </div>
-        <div class="h-15 flex">
-            <img class="object-contain  h-10  w-full" src="{{ asset('imagenes/izquierdo_abajo.png') }}" alt="">
-            <img class="object-contain  h-10  w-full" src="{{ asset('imagenes/derecho_abajo.png') }}" alt="">
-        </div>
-        <div>
-            <div class="h-40 flex  flex-4 justify-center items-center">
-            <img class="object-contain  h-24 w-full w-full" src="{{ asset('imagenes/cempasuchil.png') }}" alt="">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="/js/validacion.js"></script> 
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script type="module" src="script.js"></script>
+        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script> 
+
+    <title>JGL Gracias</title>
+</head>
+<body>
+    <img src="../public/imagenes/jgl1.jpg" alt="">
+    <div class="container  my-10 px-6 mx-auto">
+        <section  class="mb-32 text-gray-800">
+            <div class="relative overflow-hidden bg-no-repeat bg-cover"
+                 style="background-position: 50%; background-image: url('../public/imagenes/.jpg'); height: 100px;">
             </div>
-        </div>
-    </body>
+            <div class="container text-gray-800 px-4 md:px-12">
+                <div class="block rounded-lg shadow-lg py-10 md:py-12 px-2 md:px-6"
+                     style="margin-top: -100px; background: hsla(224, 43%, 100%, 0.339); backdrop-filter: blur(20px);">
+                    <div class="">
+                        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                            <h2 class=" mb-4 text-4xl tracking-tight font-extrabold text-center text-blue-900 dark:text-white">¡Gracias!</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
 </html>
